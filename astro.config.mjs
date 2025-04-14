a4023f2200742fec512be67d9b2c11a67deee457
jjmash <jeffjosephpaulwork@gmail.com>
2025-04-14 14:52:48 +0530

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://your-github-username.github.io/astro-portfolio-site/',
-  base: '/',
-  output: 'static'
-});
